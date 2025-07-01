b40440078d95697214c7ef204010cecfea08d461
Kristijan Vrabec <Vrabec@agenda-software.de>
2025-07-01 07:51:39 +0000

    feat: add comparison tests for BasketItem class to enhance functionality validation

--- tests/WebApp.UnitTests/BasketItemTest.cs
+++ tests/WebApp.UnitTests/BasketItemTest.cs
@@ -427,5 +427,423 @@ namespace eShop.WebApp.Services.Tests
         }
 
         #endregion
+
+        #region Compare Functionality Tests (TDD)
+
+        [TestMethod]
+        public void CompareTo_ReturnsNegative_WhenCurrentItemPriceIsLower()
+        {
+            // Arrange
+            var item1 = new BasketItem
+            {
+                Id = "item1",
+                ProductId = 1,
+                ProductName = "Cheaper Item",
+                UnitPrice = 10.0m,
+                OldUnitPrice = 12.0m,
+                Quantity = 1
+            };
+
+            var item2 = new BasketItem
+            {
+                Id = "item2",
+                ProductId = 2,
+                ProductName = "Expensive Item",
+                UnitPrice = 20.0m,
+                OldUnitPrice = 22.0m,
+                Quantity = 1
+            };
+
+            // Act
+            var result = item1.CompareTo(item2);
+
+            // Assert
+            Assert.IsTrue(result < 0, "CompareTo should return negative when current item price is lower");
+        }
+
+        [TestMethod]
+        public void CompareTo_ReturnsPositive_WhenCurrentItemPriceIsHigher()
+        {
+            // Arrange
+            var item1 = new BasketItem
+            {
+                Id = "item1",
+                ProductId = 1,
+                ProductName = "Expensive Item",
+                UnitPrice = 25.0m,
+                OldUnitPrice = 27.0m,
+                Quantity = 1
+            };
+
+            var item2 = new BasketItem
+            {
+                Id = "item2",
+                ProductId = 2,
+                ProductName = "Cheaper Item",
+                UnitPrice = 15.0m,
+                OldUnitPrice = 17.0m,
+                Quantity = 1
+            };
+
+            // Act
+            var result = item1.CompareTo(item2);
+
+            // Assert
+            Assert.IsTrue(result > 0, "CompareTo should return positive when current item price is higher");
+        }
+
+        [TestMethod]
+        public void CompareTo_ReturnsZero_WhenItemPricesAreEqual()
+        {
+            // Arrange
+            var item1 = new BasketItem
+            {
+                Id = "item1",
+                ProductId = 1,
+                ProductName = "Item A",
+                UnitPrice = 15.50m,
+                OldUnitPrice = 17.0m,
+                Quantity = 1
+            };
+
+            var item2 = new BasketItem
+            {
+                Id = "item2",
+                ProductId = 2,
+                ProductName = "Item B",
+                UnitPrice = 15.50m,
+                OldUnitPrice = 18.0m,
+                Quantity = 2
+            };
+
+            // Act
+            var result = item1.CompareTo(item2);
+
+            // Assert
+            Assert.AreEqual(0, result, "CompareTo should return zero when item prices are equal");
+        }
+
+        [TestMethod]
+        public void CompareTo_ThrowsArgumentNullException_WhenOtherItemIsNull()
+        {
+            // Arrange
+            var item = new BasketItem
+            {
+                Id = "item1",
+                ProductId = 1,
+                ProductName = "Test Item",
+                UnitPrice = 10.0m,
+                OldUnitPrice = 12.0m,
+                Quantity = 1
+            };
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => item.CompareTo(null));
+        }
+
+        [TestMethod]
+        public void IsCheaperThan_ReturnsTrue_WhenCurrentItemPriceIsLower()
+        {
+            // Arrange
+            var cheaperItem = new BasketItem
+            {
+                Id = "item1",
+                ProductId = 1,
+                ProductName = "Cheaper Item",
+                UnitPrice = 8.99m,
+                OldUnitPrice = 10.0m,
+                Quantity = 1
+            };
+
+            var expensiveItem = new BasketItem
+            {
+                Id = "item2",
+                ProductId = 2,
+                ProductName = "Expensive Item",
+                UnitPrice = 15.99m,
+                OldUnitPrice = 18.0m,
+                Quantity = 1
+            };
+
+            // Act
+            var result = cheaperItem.IsCheaperThan(expensiveItem);
+
+            // Assert
+            Assert.IsTrue(result, "IsCheaperThan should return true when current item price is lower");
+        }
+
+        [TestMethod]
+        public void IsCheaperThan_ReturnsFalse_WhenCurrentItemPriceIsHigherOrEqual()
+        {
+            // Arrange
+            var expensiveItem = new BasketItem
+            {
+                Id = "item1",
+                ProductId = 1,
+                ProductName = "Expensive Item",
+                UnitPrice = 20.0m,
+                OldUnitPrice = 22.0m,
+                Quantity = 1
+            };
+
+            var cheaperItem = new BasketItem
+            {
+                Id = "item2",
+                ProductId = 2,
+                ProductName = "Cheaper Item",
+                UnitPrice = 15.0m,
+                OldUnitPrice = 17.0m,
+                Quantity = 1
+            };
+
+            var equalPriceItem = new BasketItem
+            {
+                Id = "item3",
+                ProductId = 3,
+                ProductName = "Equal Price Item",
+                UnitPrice = 20.0m,
+                OldUnitPrice = 25.0m,
+                Quantity = 1
+            };
+
+            // Act
+            var resultHigher = expensiveItem.IsCheaperThan(cheaperItem);
+            var resultEqual = expensiveItem.IsCheaperThan(equalPriceItem);
+
+            // Assert
+            Assert.IsFalse(resultHigher, "IsCheaperThan should return false when current item price is higher");
+            Assert.IsFalse(resultEqual, "IsCheaperThan should return false when current item price is equal");
+        }
+
+        [TestMethod]
+        public void IsMoreExpensiveThan_ReturnsTrue_WhenCurrentItemPriceIsHigher()
+        {
+            // Arrange
+            var expensiveItem = new BasketItem
+            {
+                Id = "item1",
+                ProductId = 1,
+                ProductName = "Expensive Item",
+                UnitPrice = 25.99m,
+                OldUnitPrice = 28.0m,
+                Quantity = 1
+            };
+
+            var cheaperItem = new BasketItem
+            {
+                Id = "item2",
+                ProductId = 2,
+                ProductName = "Cheaper Item",
+                UnitPrice = 12.50m,
+                OldUnitPrice = 15.0m,
+                Quantity = 1
+            };
+
+            // Act
+            var result = expensiveItem.IsMoreExpensiveThan(cheaperItem);
+
+            // Assert
+            Assert.IsTrue(result, "IsMoreExpensiveThan should return true when current item price is higher");
+        }
+
+        [TestMethod]
+        public void IsMoreExpensiveThan_ReturnsFalse_WhenCurrentItemPriceIsLowerOrEqual()
+        {
+            // Arrange
+            var cheaperItem = new BasketItem
+            {
+                Id = "item1",
+                ProductId = 1,
+                ProductName = "Cheaper Item",
+                UnitPrice = 10.0m,
+                OldUnitPrice = 12.0m,
+                Quantity = 1
+            };
+
+            var expensiveItem = new BasketItem
+            {
+                Id = "item2",
+                ProductId = 2,
+                ProductName = "Expensive Item",
+                UnitPrice = 20.0m,
+                OldUnitPrice = 22.0m,
+                Quantity = 1
+            };
+
+            var equalPriceItem = new BasketItem
+            {
+                Id = "item3",
+                ProductId = 3,
+                ProductName = "Equal Price Item",
+                UnitPrice = 10.0m,
+                OldUnitPrice = 15.0m,
+                Quantity = 1
+            };
+
+            // Act
+            var resultLower = cheaperItem.IsMoreExpensiveThan(expensiveItem);
+            var resultEqual = cheaperItem.IsMoreExpensiveThan(equalPriceItem);
+
+            // Assert
+            Assert.IsFalse(resultLower, "IsMoreExpensiveThan should return false when current item price is lower");
+            Assert.IsFalse(resultEqual, "IsMoreExpensiveThan should return false when current item price is equal");
+        }
+
+        [TestMethod]
+        public void HasSamePrice_ReturnsTrue_WhenItemPricesAreEqual()
+        {
+            // Arrange
+            var item1 = new BasketItem
+            {
+                Id = "item1",
+                ProductId = 1,
+                ProductName = "Item A",
+                UnitPrice = 19.99m,
+                OldUnitPrice = 22.0m,
+                Quantity = 1
+            };
+
+            var item2 = new BasketItem
+            {
+                Id = "item2",
+                ProductId = 2,
+                ProductName = "Item B",
+                UnitPrice = 19.99m,
+                OldUnitPrice = 25.0m,
+                Quantity = 3
+            };
+
+            // Act
+            var result = item1.HasSamePrice(item2);
+
+            // Assert
+            Assert.IsTrue(result, "HasSamePrice should return true when item prices are exactly equal");
+        }
+
+        [TestMethod]
+        public void HasSamePrice_ReturnsFalse_WhenItemPricesAreDifferent()
+        {
+            // Arrange
+            var item1 = new BasketItem
+            {
+                Id = "item1",
+                ProductId = 1,
+                ProductName = "Item A",
+                UnitPrice = 19.99m,
+                OldUnitPrice = 22.0m,
+                Quantity = 1
+            };
+
+            var item2 = new BasketItem
+            {
+                Id = "item2",
+                ProductId = 2,
+                ProductName = "Item B",
+                UnitPrice = 20.00m,
+                OldUnitPrice = 25.0m,
+                Quantity = 1
+            };
+
+            // Act
+            var result = item1.HasSamePrice(item2);
+
+            // Assert
+            Assert.IsFalse(result, "HasSamePrice should return false when item prices are different");
+        }
+
+        [TestMethod]
+        public void CompareByTotalValue_ReturnsNegative_WhenCurrentItemTotalIsLower()
+        {
+            // Arrange
+            var item1 = new BasketItem // Total: 10.0 * 2 = 20.0
+            {
+                Id = "item1",
+                ProductId = 1,
+                ProductName = "Item A",
+                UnitPrice = 10.0m,
+                OldUnitPrice = 12.0m,
+                Quantity = 2
+            };
+
+            var item2 = new BasketItem // Total: 15.0 * 3 = 45.0
+            {
+                Id = "item2",
+                ProductId = 2,
+                ProductName = "Item B",
+                UnitPrice = 15.0m,
+                OldUnitPrice = 18.0m,
+                Quantity = 3
+            };
+
+            // Act
+            var result = item1.CompareByTotalValue(item2);
+
+            // Assert
+            Assert.IsTrue(result < 0, "CompareByTotalValue should return negative when current item total value is lower");
+        }
+
+        [TestMethod]
+        public void CompareByTotalValue_ReturnsPositive_WhenCurrentItemTotalIsHigher()
+        {
+            // Arrange
+            var item1 = new BasketItem // Total: 20.0 * 4 = 80.0
+            {
+                Id = "item1",
+                ProductId = 1,
+                ProductName = "Item A",
+                UnitPrice = 20.0m,
+                OldUnitPrice = 22.0m,
+                Quantity = 4
+            };
+
+            var item2 = new BasketItem // Total: 15.0 * 2 = 30.0
+            {
+                Id = "item2",
+                ProductId = 2,
+                ProductName = "Item B",
+                UnitPrice = 15.0m,
+                OldUnitPrice = 18.0m,
+                Quantity = 2
+            };
+
+            // Act
+            var result = item1.CompareByTotalValue(item2);
+
+            // Assert
+            Assert.IsTrue(result > 0, "CompareByTotalValue should return positive when current item total value is higher");
+        }
+
+        [TestMethod]
+        public void CompareByTotalValue_ReturnsZero_WhenItemTotalValuesAreEqual()
+        {
+            // Arrange
+            var item1 = new BasketItem // Total: 10.0 * 3 = 30.0
+            {
+                Id = "item1",
+                ProductId = 1,
+                ProductName = "Item A",
+                UnitPrice = 10.0m,
+                OldUnitPrice = 12.0m,
+                Quantity = 3
+            };
+
+            var item2 = new BasketItem // Total: 15.0 * 2 = 30.0
+            {
+                Id = "item2",
+                ProductId = 2,
+                ProductName = "Item B",
+                UnitPrice = 15.0m,
+                OldUnitPrice = 18.0m,
+                Quantity = 2
+            };
+
+            // Act
+            var result = item1.CompareByTotalValue(item2);
+
+            // Assert
+            Assert.AreEqual(0, result, "CompareByTotalValue should return zero when item total values are equal");
+        }
+
+        #endregion
     }
 }
